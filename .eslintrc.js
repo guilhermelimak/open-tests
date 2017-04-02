ddb82905b555cce26843c3106b49376178867c88
Guilherme Lima <guilhermelimak@gmail.com>
2017-04-02 18:14:57 -0300

    chore: Change eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,23 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import",
-        "jest"
-    ],
-    "rules": {
-      "semi": "off",
-      "import/no-extraneous-dependencies": "off",
-      "import/no-unresolved": "off",
-      "import/extensions": "off"
-    },
-    "env": {
-      "jest/globals": true
-    },
-    "globals": {
-      "atom": true,
-    }
+  "extends": "airbnb-base",
+  "plugins": [
+    "import",
+    "jest"
+  ],
+  "rules": {
+    "semi": [2, "never"],
+    "import/no-extraneous-dependencies": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": "off",
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "global-require": "off",
+  },
+  "env": {
+    "jest/globals": true
+  },
+  "globals": {
+    "atom": true,
+  }
 };
