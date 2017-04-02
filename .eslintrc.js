d363e3e287f39098734f4f5b6d8caed7caae3ec2
Guilherme Lima <guilhermelimak@gmail.com>
2017-04-02 12:23:46 -0300

    chore: Add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
     "extends": "airbnb-base",
     "plugins": [
-        "import"
+        "import",
+        "jest"
     ],
     "rules": {
       "semi": "off",
       "import/no-extraneous-dependencies": "off",
       "import/no-unresolved": "off",
       "import/extensions": "off"
+    },
+    "env": {
+      "jest/globals": true
     },
     "globals": {
       "atom": true,
