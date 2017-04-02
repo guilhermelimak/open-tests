0683f396fdbe38ee90dc09693a67849755ccc194
Guilherme Lima <guilhermelimak@gmail.com>
2017-04-02 12:02:00 -0300

    chore: Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "rules": {
+      "semi": "off",
+      "import/no-extraneous-dependencies": "off",
+      "import/no-unresolved": "off",
+      "import/extensions": "off"
+    },
+    "globals": {
+      "atom": true,
+    }
+};
